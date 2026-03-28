21860e5a7bb0bd99b5ea3ad460cee6993686839f
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-03-28 09:26:50 +0530

    commit

--- backend/controller/student_controller.js
+++ backend/controller/student_controller.js
@@ -57,9 +57,7 @@ export const register = async (req, res) => {
             process.env.SECRET_KEY,
             { expiresIn: "1d" }
         );
-        verifyemail(email,token).catch(err =>
-        console.log("Mail failed:", err.message)
-        );
+
         newUser.token=token
         await newUser.save();
         return res.status(200).json({
@@ -67,6 +65,9 @@ export const register = async (req, res) => {
             message: "Registered successfully. Please verify email",
             newUser:newUser
         });
+        verifyemail(email,token).catch(err =>
+        console.log("Mail failed:", err.message)
+        );
 
     } catch (error) {
         console.log(error);
