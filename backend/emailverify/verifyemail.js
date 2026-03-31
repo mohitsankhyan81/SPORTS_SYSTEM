62f0cdb459bdf8742c71961cdce74ed66a043615
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-03-31 10:35:42 +0530

    save

--- backend/emailverify/verifyemail.js
+++ backend/emailverify/verifyemail.js
@@ -2,9 +2,7 @@ import nodemailer from "nodemailer"
 
 export const verifyemail=async(email,token)=>{
     const transport=nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,          
-        secure: false, 
+        service:"gmail",
         auth:{
             user:process.env.MAIL_USER,
             pass:process.env.MAIL_PASS
