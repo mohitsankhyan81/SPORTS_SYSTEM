24020710270ab1fb235fac66d4f40adc04201b26
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-04-02 10:50:49 +0530

    commit

--- frontend/src/feature/IssueSports.jsx
+++ frontend/src/feature/IssueSports.jsx
@@ -28,7 +28,7 @@ const IssueSports = () => {
     }
 
     return (
-        <div className="bg-[url('/abigail-keenan-8-s5QuUBtyM-unsplash.jpg')]">
+        <div className="bg-[url('/abigail-keenan-8-s5QuUBtyM-unsplash.jpg')] bg-cover bg-center min-h-screen ">
             <MoveLeft size={136} strokeWidth={1.75} className="relative -top-6"/>
             <div className="bg-cover bg-center min-h-screen flex flex-col items-center justify-center p-6">
             {profile?.photo?.url && (
